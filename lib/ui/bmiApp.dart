a104a8aa7928a5cdb777f8d085639dca0aa221d5
kunledarams2 <Adekunle@1>
2020-06-11 23:07:09 +0100

    **Day One project sample ***Mass Weight Calculator***

--- lib/ui/bmiApp.dart
+++ lib/ui/bmiApp.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class BMIAppScreen extends StatefulWidget {
   @override
@@ -7,19 +8,58 @@ class BMIAppScreen extends StatefulWidget {
 }
 
 class _BMIAppScreenState extends State<BMIAppScreen> {
+  // https://www.epic4health.com/bmiformula.html
+  /**
+   * Body Mass Index can be calculated using pounds and inches with this equation
 
-  final TextEditingController _bodyEditController = new TextEditingController();
+      BMI = (	            Weight in Pounds
+      (Height in inches) x (Height in inches)	) x 703
+      For example, a person who weighs 220 pounds and is 6 feet 3 inches tall has a BMI of 27.5.
+
+      (	            220 lbs.
+      (75 inches) x (75 inches)	) x 703 = 27.5
+
+   **/
+
+  final TextEditingController _ageEditController = new TextEditingController();
   final TextEditingController _heightEditController = new TextEditingController();
   final TextEditingController _weightEditController = new TextEditingController();
   double result= 0.0;
+  double inches = 0.0;
+  var resultValue = "";
+  String _finalResult = "";
 
   void calculateBMi(){
     setState(() {
-      result = double.parse(_bodyEditController.text.toString())*
-          double.parse(_heightEditController.text.toString()) *
-          double.parse(_weightEditController.text.toString()) ;
+      int mAge = int.parse(_ageEditController.text);
+      double mHeight = double.parse(_heightEditController.text);
+      double mWeight = double.parse(_weightEditController.text);
+      inches = mHeight * 12;
+      if ((_ageEditController.text.isNotEmpty || mAge > 0) &&
+          ((_heightEditController.text.isNotEmpty || mWeight > 0) &&
+              (_weightEditController.text.isNotEmpty || inches > 0))) {
+        result = mWeight / (inches * inches) * 703;
+
+        if (double.parse(result.toStringAsFixed(1)) < 18.5) {
+          resultValue = "Underweight";
+        } else if (double.parse(result.toStringAsFixed(1)) >= 18.5 &&
+            result < 25) {
+          resultValue = "Great Shape";
+        } else if (double.parse(result.toStringAsFixed(1)) >= 25 &&
+            result < 30) {
+          resultValue = "Overweight";
+        } else if (double.parse(result.toStringAsFixed(1)) >= 30) {
+          resultValue = "Obese";
+        }
+      } else {
+        Fluttertoast.showToast(
+            msg: "Please enter all the details",
+            toastLength: Toast.LENGTH_LONG);
+        result = 0.0;
+      }
     });
 
+    _finalResult = "Your BMI is: ${double.parse(result.toStringAsFixed(1))}";
   }
 
 
@@ -58,7 +98,7 @@ class _BMIAppScreenState extends State<BMIAppScreen> {
                     Padding(
                       padding: const EdgeInsets.only(right:10.0,left: 10, top: 8),
                       child: TextField(
-                        controller: _bodyEditController,
+                        controller: _ageEditController,
                         decoration: InputDecoration(
                           labelText: "Age",
                           icon: Icon(Icons.person_outline),
@@ -112,7 +152,12 @@ class _BMIAppScreenState extends State<BMIAppScreen> {
             ),
             Container(
               margin: EdgeInsets.only(top: 20),
-              child: Text("$result"),)
+              child: Text("$_finalResult",
+                style: TextStyle(color: Colors.red, fontSize: 22),),),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Text("$resultValue",
+                style: TextStyle(color: Colors.purpleAccent, fontSize: 20),),),
           ],
         ),
       ),
